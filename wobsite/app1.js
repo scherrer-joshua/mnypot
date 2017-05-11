5844202041f08e45f6c60f7797769909c142150d
asciarretta1 <asciarretta1@gmail.com>
2017-05-10 22:28:51 -0400

    Changes to filing to make back end happen

--- wobsite/app1.js
+++ wobsite/app1.js
@@ -2,12 +2,32 @@ var mustacheExpress = require('mustache-express');
 var express = require('express');
 var app = express()
 
+app.use(express.static('static'))
+
 app.engine("html", mustacheExpress());
 
 app.set("view engine", "html");
 
 app.get('/', function (req, res) {
   res.render('InitialDropDown');
+})
+
+app.get('/taxdata', function (req, res) {
+  var data = [1,1,1,1,1,1,1,1,1,1,1]; 
+
+  res.render('home', {
+    data: JSON.stringify(data),
+    statename: req.param("state")
+  });
+})
+
+app.get('/closing', function (req, res) {
+  res.render('ClosingPage');
+})
+
+app.get('/userdata', function (req, res) {
+  var data = [10,10,15,15,5,5,0,0,0,20,20];
+  res.render('OurCollectedDataVis', {data: JSON.stringify(data)});
 })
 
 app.listen(3000, function () {
